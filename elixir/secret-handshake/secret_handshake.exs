e1b84ad4b602e9235d268bc82b18e695931a7434
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-05-21 22:04:08 +0200

    Implements first solution for secret-handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,8 @@
 defmodule SecretHandshake do
+  use Bitwise
+
+  @binary_map  %{0b1 => "wink", 0b10 => "double blink", 0b100 => "close your eyes", 0b1000 => "jump"}
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,5 +19,19 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    secret = Enum.filter(@binary_map, fn({key, _val}) -> set?(code, key) end)
+    |> Enum.map(fn({_key, val}) -> val end)
+
+    if reverse?(code), do: Enum.reverse(secret), else: secret
+  end
+
+  @spec set?(flags :: integer, check :: integer) :: boolean()
+  def set?(flags, check) do
+    band(flags, check) == check
+  end
+
+  @spec reverse?(code :: integer) :: boolean()
+  def reverse?(code) do
+    set?(code, 0b10000)
   end
 end
