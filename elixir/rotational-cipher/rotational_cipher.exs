2c07a4bd738707e85a65298885bbe3bf426bfef8
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-05-23 09:31:36 +0200

    Implements rotational-cipher with streams

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,5 +8,19 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    for <<c <- text>> do
+      cond do
+        c in ?a..?z -> <<shifter(c, shift, ?a..?z)>>
+        c in ?A..?Z -> <<shifter(c, shift, ?A..?Z)>>
+        true -> <<c>>
+      end
+    end |> Enum.join()
+  end
+  
+  @spec shifter(from :: integer, shift :: integer, within :: Range.t()) :: integer
+  defp shifter(from, shift, within) do
+    first.._last = within
+    Stream.cycle(within)
+    |> Enum.at(from + shift - first)  # Enum.at/1 looks up by *index*
   end
 end
