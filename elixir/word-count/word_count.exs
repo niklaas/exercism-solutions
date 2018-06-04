d10c1222e0694993f826191e33bd0e43d037be65
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-06-04 23:24:10 +0200

    Implements exercise word-count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,5 +6,12 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    list_of_words =
+      sentence
+      |> String.downcase()
+      |> String.split([" ", "_", "@", "%", ".", "!", "?", "#", ",", "$", "^", "\"", ":", "&"], trim: true)
+      |> Enum.group_by(&(&1))
+      |> Enum.map(fn {k, v} -> {k, Enum.count(v)} end)
+      |> Map.new()
   end
 end
