365679e7c68899fc890f3bb731880b9f3ba8bdcf
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-06-17 21:37:55 +0200

    Implements exercise accumulate with `Enum.reduce/3`

--- elixir/accumulate/accumulate.exs
+++ elixir/accumulate/accumulate.exs
@@ -17,21 +17,8 @@ defmodule Accumulate do
 
   @spec accumulate(list, (any -> any)) :: list
   def accumulate(list, fun) do
-    do_accumulate(list, fun, [])
-  end
-
-  @spec do_accumulate(list :: list, fun :: (any -> any), list) :: list
-  defp do_accumulate(list, fun, acc)
-
-  defp do_accumulate([], _fun, []) do
-    []
-  end
-
-  defp do_accumulate([], _fun, acc) do
-    Enum.reverse(acc)
-  end
-
-  defp do_accumulate([head | tail], fun, acc) do
-    do_accumulate(tail, fun, [fun.(head) | acc])
+    list
+    |> Enum.reduce([], fn x, acc -> [fun.(x) | acc] end)
+    |> Enum.reverse()
   end
 end
