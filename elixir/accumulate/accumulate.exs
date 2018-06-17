c1abd1835d4bbd425a4886b27b21b9a20823b435
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-06-17 21:15:01 +0200

    Solves exercise accumulate for elixir

--- elixir/accumulate/accumulate.exs
+++ elixir/accumulate/accumulate.exs
@@ -17,5 +17,21 @@ defmodule Accumulate do
 
   @spec accumulate(list, (any -> any)) :: list
   def accumulate(list, fun) do
+    do_accumulate(list, fun, [])
+  end
+
+  @spec do_accumulate(list :: list, fun :: (any -> any), list) :: list
+  defp do_accumulate(list, fun, acc)
+
+  defp do_accumulate([], _fun, []) do
+    []
+  end
+
+  defp do_accumulate([], _fun, acc) do
+    Enum.reverse(acc)
+  end
+
+  defp do_accumulate([head | tail], fun, acc) do
+    do_accumulate(tail, fun, [fun.(head) | acc])
   end
 end
