a000d62fb506a464164d5a38369f5ca726d875ed
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-06-18 23:01:14 +0200

    Implements solution for exercise acronyms

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,5 +5,30 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split([" ", "-"], trim: true)
+    |> Enum.map(&do_abbreviate/1)
+    |> Enum.join("")
+    |> String.upcase()
+  end
+
+  @spec do_abbreviate(word :: String.t()) :: String.t()
+  defp do_abbreviate(word)
+
+  defp do_abbreviate(<<first, rest::binary>>) do
+    <<first>> <> uppercase_only(rest)
+  end
+
+  @spec uppercase_only(phrase :: String.t()) :: String.t()
+  defp uppercase_only("") do
+    ""
+  end
+
+  defp uppercase_only(<<letter, rest::binary>>) when letter in ?A..?Z do
+    <<letter>> <> uppercase_only(rest)
+  end
+
+  defp uppercase_only(<<_letter, rest::binary>>) do
+    uppercase_only(rest)
   end
 end
