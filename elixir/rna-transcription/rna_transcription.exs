216d5a839015220fc366c36a52d9c0bbeb9d5e5d
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-09-09 21:39:15 +0200

    Cleans up solution for rna-transcription

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -1,5 +1,5 @@
 defmodule RNATranscription do
-  @translator %{?G => 'C', ?C => 'G', ?T => 'A', ?A => 'U'}
+  @translator %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
 
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
@@ -15,10 +15,10 @@ defmodule RNATranscription do
   end
 
   defp do_to_rna([]) do
-    ''
+    []
   end
 
   defp do_to_rna([head | tail]) do
-    Map.get(@translator, head) ++ do_to_rna(tail)
+    [Map.get(@translator, head) | do_to_rna(tail)]
   end
 end
