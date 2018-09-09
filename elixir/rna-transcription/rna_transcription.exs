be7e629a3dc8bd9e91317b730c35bcfed5fe059a
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-09-09 17:45:57 +0200

    Adds solution for exercise rna-transcriptions

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,24 @@
+defmodule RNATranscription do
+  @translator %{?G => 'C', ?C => 'G', ?T => 'A', ?A => 'U'}
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    do_to_rna(dna)
+  end
+
+  defp do_to_rna([]) do
+    ''
+  end
+
+  defp do_to_rna([head | tail]) do
+    Map.get(@translator, head) ++ do_to_rna(tail)
+  end
+end
