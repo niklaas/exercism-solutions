fbdff7a55817e297f1c80f58d1ceda9a8f03275c
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-09-23 14:57:03 +0200

    Implements solution for exercise raindrops

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -1,4 +1,10 @@
 defmodule Raindrops do
+  @raindrops [
+    {3, "Pling"},
+    {5, "Plang"},
+    {7, "Plong"}
+  ]
+
   @doc """
   Returns a string based on raindrop factors.
 
@@ -10,5 +16,23 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t()
   def convert(number) do
+    do_sound(number, @raindrops, "")
   end
+
+  defp do_sound(number, [head | tail], acc) when rem(number, elem(head, 0)) == 0 do
+    do_sound(number, tail, acc <> elem(head, 1))
+  end
+
+  defp do_sound(number, [_head | tail], acc) do
+    do_sound(number, tail, acc)
+  end
+
+  defp do_sound(number, [], "") do
+    Integer.to_string(number)
+  end
+
+  defp do_sound(_number, [], acc) do
+    acc
+  end
+
 end
