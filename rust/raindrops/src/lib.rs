9ed75243b0d75813a6bf53b4802d0c9696534d83
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-10-28 17:52:24 +0100

    Implements solution for raindrop

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut out = String::new();
+
+    if n % 3 == 0 {
+        out.push_str("Pling");
+    }
+
+    if n % 5 == 0 {
+        out.push_str("Plang");
+    }
+
+    if n % 7 == 0 {
+        out.push_str("Plong");
+    }
+
+    if out.is_empty() {
+        out = n.to_string();
+    }
+
+    out
 }
