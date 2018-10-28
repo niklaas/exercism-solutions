73672b607a991fab285a18ae3af2e74200d7d4b1
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-10-28 12:45:22 +0100

    Adds solution to exercise gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,10 @@
 extern crate chrono;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let d = Duration::seconds(10i64.pow(9));
+    start
+        .checked_add_signed(d)
+        .expect("could not add a gigasecond")
 }
