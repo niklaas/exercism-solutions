6fbecbcc9e282009ce496ed1798c99e9af0b6ad3
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2018-10-28 10:04:08 +0100

    Implements solution for pythagorean-triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    let s = 1000;
+
+    for a in 1u32..s {
+        for b in 1u32..s {
+            for c in 1u32..s {
+                if a < b && b < c && a.pow(2) + b.pow(2) == c.pow(2) && a + b + c == s {
+                    return Some(a * b * c);
+                }
+            }
+        }
+    }
+
+    None
 }
